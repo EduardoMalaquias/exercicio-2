3a067686504fa5709c8d179e6e02dbff2aae4931
Eduardo Malaquias <64786771+EduardoMalaquias@users.noreply.github.com>
2021-09-30 13:42:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,27 @@ function pegaUrl(url) {
     return request.responseText
 }
 
+function criaLinha(pais) {
+    linha = document.createElement("tr")
+    tdCode = document.createElement("td")
+    tdNome = document.createElement("td")
+    tdFronteira = document.createElement("td")
+
+    tdCode.innerHTML = pais.code
+    tdNome.innerHTML = pais.name
+    tdFronteira.innerHTML = pais.fronteiras
+
+    linha.appendChild(tdCode)
+    linha.appendChild(tdNome)
+    linha.appendChild(tdFronteira)
+
+    return linha
+}
+
 function main () {
     let data = pegaUrl("http://www.amock.io/api/fcmaia/countries")
     let paises = JSON.parse(data)
-    
+    let tabela = document.getElementById("tabela")
     paises.sort(function(a, b){
         if (a.fronteiras.length < b.fronteiras.length){
             return 1
@@ -19,7 +36,10 @@ function main () {
             return 0
         }
     });
-    console.log(paises)
+    paises.forEach(element => {
+        let linha = criaLinha(element)
+        tabela.appendChild(linha)
+    });
 }
 
 main()
